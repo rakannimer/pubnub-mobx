569369bead96ebf9ced764478181bc9a8edd7d74
rakannimer <rakannimer@gmail.com>
2018-10-07 17:02:14 +0300

    test: 💍 Fix tests. Add warning

--- src/credentials.sample.ts
+++
@@ -1,5 +0,0 @@
-// Rename to credentials.ts
-export const credentials = {
-  publishKey: "REPLACE_WITH_YOUR_PUBNUB_PUBLISH_KEY",
-  subscribeKey: "REPLACE_WITH_YOUR_PUBNUB_SUBSCRIBE_KEY"
-};
---
+++ src/credentials.ts
@@ -0,0 +1,6 @@
+export const credentials = {
+  // These credentials are only for demo purposes.
+  // Do not re-use or rely on them for any serious work.
+  publishKey: "pub-c-ba511f97-2a71-48d6-bdda-342c7249a6be",
+  subscribeKey: "sub-c-89702f8c-c9fa-11e8-bbf2-f202706b73e5"
+};
